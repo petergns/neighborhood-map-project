c58de5c437adf65c05466d9c45ce416614ad4680
petergns <petergns@outlook.com>
2018-03-16 17:41:08 -0400

    Remove Testing Files

--- js/app.js
+++
@@ -1,8 +0,0 @@
-      var map;
-      function initMap() {
-        // Constructor creates a new map - only center and zoom are required.
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 38.899317, lng: -77.020963},
-          zoom: 16
-        });
-      }
