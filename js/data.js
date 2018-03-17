7cb3b3898e2f8ab1c4881108f025b996b299fb02
petergns <petergns@outlook.com>
2018-03-17 00:47:04 -0400

    Add Location Data

--- js/data.js
+++ js/data.js
@@ -20,9 +20,9 @@ var locations = [{
 	id: 2
 }, {
 	title: "Smithsonian Metro Station",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "wmata.com/rider-guide/stations/smithsonian.cfm?y=12",
+	street: "1278-1160 Jefferson Dr SW",
+	city: "Washington, DC 20024",
 	COORD: {
 		lat: 38.889057,
 		lng: -77.028505
@@ -30,9 +30,9 @@ var locations = [{
 	id: 3
 }, {
 	title: "Federal Triangle Metro Station",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "wmata.com/rider-guide/stations/federal-triangle.cfm?y=12",
+	street: "302 12th St. NW",
+	city: "Washington, DC 20004",
 	COORD: {
 		lat: 38.893782,
 		lng: -77.028315
@@ -40,9 +40,9 @@ var locations = [{
 	id: 4
 }, {
 	title: "L'Enfant Plaza Metro Station",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "wmata.com/rider-guide/stations/lenfant-plaza.cfm?y=47",
+	street: "600 Maryland Avenue SW",
+	city: "Washington, DC 20024",
 	COORD: {
 		lat: 38.884784,
 		lng: -77.020884
@@ -50,9 +50,9 @@ var locations = [{
 	id: 5
 }, {
 	title: "Woodley Park-Zoo Metro Station",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "wmata.com/rider-guide/stations/woodley-park.cfm?y=38",
+	street: "2700 Connecticut Ave. NW",
+	city: "Washington, DC 20008",
 	COORD: {
 		lat: 38.924519,
 		lng: -77.0523838
@@ -60,9 +60,9 @@ var locations = [{
 	id: 6
 }, {
 	title: "Navy-Yard Ballpark Station",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "wmata.com/rider-guide/stations/navy-yard.cfm",
+	street: "200 M Street SE",
+	city: "Washington, DC 20003",
 	COORD: {
 		lat: 38.876678,
 		lng: -77.004507
@@ -70,9 +70,9 @@ var locations = [{
 	id: 7
 }, {
 	title: "Rosslyn Metro Station",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "wmata.com/rider-guide/stations/rosslyn.cfm?y=12",
+	street: "61850 North Moore St",
+	city: "Arlington, VA 22209",
 	COORD: {
 		lat: 38.895805,
 		lng: -77.071909
@@ -80,9 +80,9 @@ var locations = [{
 	id: 8
 }, {
 	title: "Panera Bread, Rosslyn Center",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "locations.panerabread.com/va/arlington/1700-n--moore-street-suite-110.html",
+	street: "1700 N Moore St Suite 110",
+	city: "Arlington, VA 22209",
 	COORD: {
 		lat: 38.895588,
 		lng: -77.072040
@@ -90,9 +90,9 @@ var locations = [{
 	id: 9
 }, {
 	title: "Fort Totten Metro Station",
-	url: "starbucks.com/store-locator/store/16174/",
-	street: "800 7th St NW #305",
-	city: "Washington, DC 20001",
+	url: "wmata.com/rider-guide/stations/fort-totten.cfm?y=36",
+	street: "340 Galloway St NE",
+	city: "Washington, DC 20011",
 	COORD: {
 		lat: 38.952487,
 		lng: -77.002906
